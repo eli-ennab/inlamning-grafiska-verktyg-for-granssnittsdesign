147e5d8c67325c91f80fea409b193a66307bb436
Eli Ennab <elinaennab@gmail.com>
2023-04-12 16:10:29 +0200

    show/hide element functions and show form when cta-button is clicked

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,30 @@
+/**
+ * Queries
+ */
 const ctaBtn = document.querySelector('#cta-button') as HTMLButtonElement
+const mainContent = document.querySelector('#main-content') as HTMLDivElement
+const formContent = document.querySelector('#form-content') as HTMLDivElement
 
+/**
+ * Show element
+ */
+const showElement = (element: HTMLElement) => {
+	element.classList.remove('hide')
+}
+
+/**
+ * Hide element
+ */
+const hideElement = (element: HTMLElement) => {
+	element.classList.add('hide')
+}
+
+/**
+ * CTA Button Event 
+ */
 ctaBtn!.addEventListener('click', e => {
   e.preventDefault()
 
-  console.log('You clicked me')
+  hideElement(mainContent)
+  showElement(formContent)
 })
